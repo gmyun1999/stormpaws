b2e2fbfda50405bde46828a133b08dc07915dbe8
NamJongha <plazanimo@ajou.ac.kr>
2025-04-19 13:12:06 +0900

    GetMapping
    code를 클라이언트로 넘겨주는게 아니라 spring에서 받는 거로 끝나는거였습니다. 죄송합니다.

--- src/main/java/com/example/stormpaws/web/controller/AuthController.java
+++ src/main/java/com/example/stormpaws/web/controller/AuthController.java
@@ -7,9 +7,12 @@ import com.example.stormpaws.web.dto.OAuthCodeRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/user/login")
 public class AuthController {
 
   private final AuthService authService;
@@ -17,6 +20,15 @@ public class AuthController {
   public AuthController(AuthService authService) {
     this.authService = authService;
   }
+
+  // GET  /login/{authServer}
+  @GetMapping("/{authServer}")
+  public ResponseEntity<ApiResponse<AuthDataDto>> oauthCallback(@PathVariable String authServer, @RequestParam String code){
+    AuthDataDto authData = authService.login(authServer, code);
+    ApiResponse<AuthDataDto> response = new ApiResponse<>(true, "Login Success", authData);
+    return ResponseEntity.ok(response);
+  }
+  
 
   // POST /login/{authServer}
   @PostMapping("/{authServer}")

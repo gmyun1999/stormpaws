250559e42a04a7b5950508e60044978baa787e55
gmyun1999 <108219645+gmyun1999@users.noreply.github.com>
2025-04-29 16:46:46 +0900

    chore: gradle 디버거 추가 (#4)
    * chore: gradle 디버거 추가
    
    * refactor: jpa 인프라 레벨로 옮기기

---
+++ src/main/java/com/example/stormpaws/domain/IRepository/IUserRepository.java
@@ -0,0 +1,14 @@
+package com.example.stormpaws.domain.IRepository;
+
+import com.example.stormpaws.domain.model.UserModel;
+import java.util.Optional;
+
+public interface IUserRepository {
+  UserModel save(UserModel user);
+
+  Optional<UserModel> findByOauthTypeAndOauthId(String oauthType, String oauthId);
+
+  Optional<UserModel> findByEmail(String email);
+
+  Optional<UserModel> findById(String id);
+}

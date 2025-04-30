87ae07fc1dfcee4b9867f7ae8666dbbc25d9b7c2
gmyun1999 <dbsrbals26@gmail.com>
2025-05-01 03:14:09 +0900

    feat: controller 스켈레톤

---
+++ src/main/java/com/example/stormpaws/web/controller/DeckController.java
@@ -0,0 +1,21 @@
+package com.example.stormpaws.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class DeckController {
+
+  // @GetMapping("/decks/{deckId}")
+  // public ResponseEntity<?> getDeckByID(@PathVariable String deckId) {}
+
+  // @GetMapping("/decks/random-opponents")
+  // public ResponseEntity<?> getRandomDeck() {}
+
+  // @GetMapping("/user/me/decks")
+  // public ResponseEntity<?> getMyDecks() {}
+
+  // @PostMapping("/user/me/decks")
+  // public ResponseEntity<?> createDeck() {}
+}

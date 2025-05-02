5507354437f5bfa18efd48597b252f1659a05843
NamJongha <146957410+NamJongha@users.noreply.github.com>
2025-05-02 11:25:20 +0900

    JWT토큰 Post로 클라이언트에 넘겨주기 (#7)
    * 패키지 대소문자
    
    일부 수정
    
    * fix
    
    * 클라이언트 토큰 넘겨주기 및 예외 처리 일부 추가
    
    * Update OAuthCodeRequest.java
    
    @Getter -> @Data와 중복
    
    * Update UserController.java

--- src/main/java/com/example/stormpaws/service/exception/OAuthException.java
+++ src/main/java/com/example/stormpaws/service/exception/OAuthException.java
@@ -2,11 +2,24 @@ package com.example.stormpaws.service.exception;
 
 public class OAuthException extends RuntimeException {
 
+  private String errorResponse;
+
   public OAuthException(String message) {
     super(message);
   }
 
   public OAuthException(String message, Throwable cause) {
     super(message, cause);
+  }
+
+  // 오류 응답을 포함하는 생성자
+  public OAuthException(String message, String errorResponse) {
+    super(message);
+    this.errorResponse = errorResponse;  // Google API에서 반환한 에러 응답을 저장
+  }
+
+// 오류 응답을 반환하는 getter
+  public String getErrorResponse() {
+    return errorResponse;
   }
 }
--- src/main/java/com/example/stormpaws/web/dto/OAuthCodeRequest.java
+++ src/main/java/com/example/stormpaws/web/dto/OAuthCodeRequest.java
@@ -3,8 +3,10 @@ package com.example.stormpaws.web.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
+@Setter
 @NoArgsConstructor
 public class OAuthCodeRequest {
   @NotBlank(message = "Authorization code must not be blank")

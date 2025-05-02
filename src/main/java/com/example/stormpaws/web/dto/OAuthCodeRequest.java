b35eca500ddb60835172fb9467a7e86bd1f78319
NamJongha <146957410+NamJongha@users.noreply.github.com>
2025-05-02 11:06:53 +0900

    Update OAuthCodeRequest.java
    @Getter -> @Data와 중복

--- src/main/java/com/example/stormpaws/web/dto/OAuthCodeRequest.java
+++ src/main/java/com/example/stormpaws/web/dto/OAuthCodeRequest.java
@@ -2,12 +2,10 @@ package com.example.stormpaws.web.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Data
-@Getter
 @Setter
 @NoArgsConstructor
 public class OAuthCodeRequest {

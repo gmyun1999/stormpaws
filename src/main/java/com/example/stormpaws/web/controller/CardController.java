f4066c1b8535d47564d322763dbe0a8ac628ecb6
dan0205 <108571937+dan0205@users.noreply.github.com>
2025-05-05 17:14:05 +0900

    Feat: make getCardById() in controller, service (#13)

--- src/main/java/com/example/stormpaws/web/controller/CardController.java
+++ src/main/java/com/example/stormpaws/web/controller/CardController.java
@@ -2,10 +2,17 @@ package com.example.stormpaws.web.controller;
 
 import com.example.stormpaws.domain.model.CardModel;
 import com.example.stormpaws.service.CardService;
+
+import com.example.stormpaws.web.dto.response.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import com.example.stormpaws.service.dto.PagedResultDTO;
 import com.example.stormpaws.web.dto.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,5 +41,10 @@ public class CardController {
 
   // @GetMapping("/{cardId}")
   // public ResponseEntity<?> getCardByID(@PathVariable String cardId) {}
-
+  @GetMapping("/{cardId}")
+  public ResponseEntity<ApiResponse<CardModel>> getCardById(@PathVariable String cardId) {
+    CardModel card = cardService.getCardById(cardId); // CardModel을 직접 받음
+    ApiResponse<CardModel> response = new ApiResponse<>(true, "success", card);
+    return ResponseEntity.ok(response);
+  }
 }

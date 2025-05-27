a2842579d036031161036953a5292cba6f5ea694
yidj0205 <yidj0205@ajou.ac.kr>
2025-05-28 02:41:43 +0900

    WeatherType 수정2

--- src/main/java/com/example/stormpaws/domain/constant/WeatherType.java
+++ src/main/java/com/example/stormpaws/domain/constant/WeatherType.java
@@ -17,14 +17,14 @@ public enum WeatherType {
     return switch (code) {
       case 0 -> CLEAR;
       case 1, 2, 3 -> CLOUDS;
-      case 45 -> MIST; // 안개
-      case 48 -> FOG; // 짙은 안개
+      case 45 -> MIST;
+      case 48 -> FOG;
       case 51, 53, 55, 61, 63, 65, 80, 81, 82 -> RAIN;
       case 71, 73, 75, 77 -> SNOW;
       case 95, 96, 99 -> THUNDERSTORM;
-      case 6 -> DUST; // 먼지
-      case 7 -> GUST; // 강풍
-      case 8, 9 -> TORNADO; // 회오리
+      case 6 -> DUST;
+      case 7 -> GUST;
+      case 8, 9 -> TORNADO;
       default -> UNKNOWN;
     };
   }

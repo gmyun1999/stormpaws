3d098f7d8804ed504cc1a06dcaeaba53f3940559
gmyun1999 <dbsrbals26@gmail.com>
2025-06-09 00:28:42 +0900

    chore: rest_client

--- src/main/java/com/example/stormpaws/service/SystemUserService.java
+++ src/main/java/com/example/stormpaws/service/SystemUserService.java
@@ -2,7 +2,9 @@ package com.example.stormpaws.service;
 
 import com.example.stormpaws.domain.IRepository.IUserRepository;
 import com.example.stormpaws.domain.model.UserModel;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SystemUserService {
   private static final String SYSTEM_USER_ID = "00000000-0000-0000-0000-000000000001";
   private final IUserRepository userRepository;

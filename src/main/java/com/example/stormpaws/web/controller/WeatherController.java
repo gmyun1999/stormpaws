34e80d994eb70425bccbdd532adc3777bd5ee1b1
gmyun1999 <dbsrbals26@gmail.com>
2025-06-08 22:57:06 +0900

    chore

--- src/main/java/com/example/stormpaws/web/controller/WeatherController.java
+++ src/main/java/com/example/stormpaws/web/controller/WeatherController.java
@@ -4,6 +4,7 @@ import com.example.stormpaws.domain.model.WeatherLogModel;
 import com.example.stormpaws.service.WeatherService;
 import com.example.stormpaws.web.dto.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +19,12 @@ public class WeatherController {
     this.weatherService = weatherService;
   }
 
-  // @GetMapping("/test_update")
-  // public ResponseEntity<String> fetchWeather() {
-  //   weatherService.updateAllCitiesWeather(); // 내부 로직은 그대로 사용
-  //   return ResponseEntity.ok("Weather data fetched and saved");
-  // }
+  @PreAuthorize("isAuthenticated()")
+  @GetMapping("/initialize")
+  public ResponseEntity<String> fetchWeather() {
+    weatherService.updateAllCitiesWeather(); // 내부 로직은 그대로 사용
+    return ResponseEntity.ok("Weather data fetched and saved");
+  }
 
   @GetMapping("/random")
   public ResponseEntity<ApiResponse<WeatherLogModel>> getRandomCityWeather() {
